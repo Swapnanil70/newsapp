416538f7c6ec0be36f8e4411196ec4adaaac5e43
Swapnanil Goswami <goswamibabin7@gmail.com>
2024-02-13 01:58:02 +0530

    Added the dynamic

--- src/components/News.js
+++ src/components/News.js
@@ -2,53 +2,69 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles =[
-      {
-          "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-          "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }
-  ]
   constructor(){
     super(); // Whenever we create a constructor in a class based component we need to call super() method to call the constructor of the parent class
-    console.log("Hello I am a constructor from NewsItem component");
     this.state = {
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0
+    }
+  }
+
+  async componentDidMount(){ // This method is called after the component is mounted (Learn about JS Promises to understand async/await)
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=7fa53e49e8794638a5940ea59fb96ece&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles : parsedData.articles, totalResults: parsedData.totalResults});
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7fa53e49e8794638a5940ea59fb96ece&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles : parsedData.articles
+    });
+  }
+
+  handleNextClick = async () => {
+    console.log("Next");
+
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){ 
+      // Do nothing and pass
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7fa53e49e8794638a5940ea59fb96ece&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles : parsedData.articles
+      });
     }
   }
 
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1>NewsMonkey - Top Headlines</h1>
         <div className="row">
           {this.state.articles.map((element)=>{
             return <div className="col md-4" key={element.url}>
-            <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            {/* Add a default image here */}
+            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/> 
           </div>
           })}
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+        <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
